2f35ead92af63a7a47c8609f406660fe473d37ad
Alexmathai2001 <alexmathai07@gmail.com>
2024-05-13 18:31:55 +0530

    second commmit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+    return (
+        <div>Hello customer</div>
+    )
+}
+
+export default page
---
+++ app/testroute/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+    <div>
+        <h1 className="font-semibold text-red-500">Alex mathai</h1>
+    </div>
+);
+  }
